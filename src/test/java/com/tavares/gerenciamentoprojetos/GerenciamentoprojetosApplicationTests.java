1b192137f098dfd330b0c1059ca5e9690f9f8df0
Andre Tavares <andreetavaress1@gmail.com>
2026-03-24 16:53:11 -0300

    add: classe Projeto criada

---
+++ src/test/java/com/tavares/gerenciamentoprojetos/GerenciamentoprojetosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tavares.gerenciamentoprojetos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoprojetosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
