de506a057c8e96ae1ed67cb2e2db0106e03c4e27
Andre Tavares <andreetavaress1@gmail.com>
2026-03-24 16:54:04 -0300

    add: repository Projeto criado

---
+++ src/main/java/com/tavares/gerenciamentoprojetos/repositories/ProjetoRepository.java
@@ -0,0 +1,8 @@
+package com.tavares.gerenciamentoprojetos.repositories;
+
+import com.tavares.gerenciamentoprojetos.models.Projeto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjetoRepository extends JpaRepository<Projeto, Long> {}
